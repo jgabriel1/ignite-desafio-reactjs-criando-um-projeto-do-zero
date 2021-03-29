ce464ff2fd7c6eef96861acb1fde269a1b961c4a
jgabriel1 <josegabriel@det.ufc.br>
2021-03-28 23:14:35 -0300

    Created home page structure and style.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { FiCalendar as CalendarIcon, FiUser as UserIcon } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,61 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main className={`${commonStyles.content} ${styles.content}`}>
+        <ul className={styles.postsList}>
+          <li>
+            <h1>Como utilizar Hooks</h1>
+
+            <p>Pensando em sincronização de ciclos de vida.</p>
+
+            <div className={styles.postInfoContainer}>
+              <div>
+                <CalendarIcon />
+
+                <time>15 Mar 2021</time>
+              </div>
+
+              <div>
+                <UserIcon />
+
+                <span>Joseph Oliveira</span>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <h1>Como utilizar Hooks</h1>
+
+            <p>Pensando em sincronização de ciclos de vida.</p>
+
+            <div className={styles.postInfoContainer}>
+              <div>
+                <CalendarIcon />
+
+                <time>15 Mar 2021</time>
+              </div>
+
+              <div>
+                <UserIcon />
+
+                <span>Joseph Oliveira</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+
+        <button type="button">Carregar mais posts</button>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
